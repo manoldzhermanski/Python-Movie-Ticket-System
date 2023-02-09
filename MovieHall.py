4c2d473bd1621847404dc7abfb9da272e735ff58
Manol <moncho.dzhermanski@abv.bg>
2023-02-10 00:17:07 +0200

    Removed unused methods

--- MovieHall.py
+++ MovieHall.py
@@ -1,91 +1,73 @@
+import os
+import psycopg2
+
+
 class MovieHall:
     __hall = []
     __rows = -1
     __seats = -1
-    __hall_number = -1
 
-    def __set_rows(self, rows):
-        if rows <= 0:
-            raise ValueError
-        else:
-            self.__rows = rows
+    __conn = psycopg2.connect(host=os.environ['DATABASE_URL'], database=os.environ['PYTHON_PROJECT_USERNAME'],
+                              password=os.environ['PASSWORD'],
+                              user=os.environ['PYTHON_PROJECT_USERNAME'])
 
-    def get_rows(self):
-        return self.__rows
+    __cursor = __conn.cursor()
 
-    def del_rows(self):
-        del self.__rows
+    def string_to_hall(self, string_hall):
+        temp = [string_hall[idx: idx + 20] for idx in range(0, len(string_hall), 20)]
+        res = list(map(lambda ele: list(map(int, ele)), temp))
+        self.__rows = len(res)
+        self.__seats = 20
+        self.__hall = res
+        return res
 
-    def __set_seats(self, seats):
-        if seats <= 0:
-            raise ValueError
-        else:
-            self.__seats = seats
+    def __validate_hall_input(self):
+        row = ""
+        seat = ""
+        while row == "":
+            row = input(f"Enter a row number (between 1 and {self.__rows}): ")
+            if row == "quit":
+                return
+            elif row.isnumeric() is False:
+                print("Error: Non-numeric input. Try again...")
+                row = ""
+            elif int(row) < 0 or int(row) > self.__rows:
+                print("Error: Invalid row. Try again...")
+                row = ""
+        while seat == "":
+            seat = input(f"Enter a seat number (between 1 and {self.__seats}): ")
+            if seat == "quit":
+                return
+            elif seat.isnumeric() is False:
+                print("Error: Non-numeric input. Try again...")
+                seat = ""
+            elif int(seat) < 0 or int(seat) > self.__seats:
+                print("Error: Invalid seat. Try again...")
+                seat = ""
+        return int(row), int(seat)
 
-    def get_seats(self):
-        return self.__seats
-
-    def del_seats(self):
-        del self.__seats
-
-    def __set_hall(self):
-        for i in range(self.__rows):
-            self.__hall.append([0] * self.__seats)
-
-    def get_hall(self):
-        return self.__hall
-
-    def del_hall(self):
-        for row in range(len(self.__hall)):
-            self.__hall.pop()
-
-    def __set_hall_number(self, hall_number):
-        self.__hall_number = hall_number
-
-    def get_hall_number(self):
-        return self.__hall_number
-
-    def __init__(self, rows, seats, hall_number):
-        try:
-            self.__set_rows(rows)
-        except ValueError:
-            print("Invalid row. Please enter a positive number")
-            raise ValueError
-        try:
-            self.__set_seats(seats)
-        except ValueError:
-            print("Invalid seat. Please enter a positive number")
-            raise ValueError
-        self.__set_hall_number(hall_number)
-        self.__set_hall()
-
-    def __repr__(self):
-        return f"Hall[{self.get_rows()}][{self.get_seats()}]"
+    def hall_to_string(self):
+        string_hall = [[str(ele) for ele in sub] for sub in self.__hall]
+        flat_list = [item for sublist in string_hall for item in sublist]
+        flat_list = "".join(flat_list)
+        return flat_list
 
     def print_hall(self):
         for row in self.__hall:
             print(*row)
 
     def buy_ticket(self):
+        print("----------------CHOOSE SEAT----------------")
+        print("To cancel the operation, type 'quit'")
         self.print_hall()
-        booked = False
-        while not booked:
-            row = int(input(f"Enter a row number (between 1 and {self.__rows})"))
-            if row < 0 or row > self.__rows:
-                print("Error: Invalid row\n")
-                continue
-            seat = int(input(f"Enter a column number (between 1 and {self.__seats})"))
-            if seat < 0 or seat > self.__seats:
-                print("Invalid seat\n")
-                continue
-            if self.__hall[row - 1][seat - 1] == 1:
-                print("This seat is already booked.")
-            else:
-                print("This seat is empty.")
-                print("Booking seat...")
-                self.__hall[row - 1][seat - 1] = 1
-                print("We have now booked this seat for you.")
-                booked = True
+        row, seat = self.__validate_hall_input()
+        if self.__hall[row - 1][seat - 1] == 1:
+            print("This seat is already booked.")
+        else:
+            print("This seat is empty.")
+            print("Buying seat...")
+            self.__hall[row - 1][seat - 1] = 1
+            print("We have now bought this seat for you.")
 
     def export_hall_info(self, file_path):
         print("Exporting hall information...")
@@ -96,12 +78,6 @@ class MovieHall:
             file.write("\n")
         print(f"Successfully exported information to {file_path}")
 
-    def reset_hall(self):
-        print("Resetting hall...")
-        self.del_hall()
-        self.__set_hall()
-        print("All seats are available")
-
     def count_free_seats(self):
         free_seats = 0
         for row in self.__hall:
@@ -110,3 +86,10 @@ class MovieHall:
             print(f"There are {free_seats} free seats available")
         else:
             print("There are no free seats available")
+
+
+if __name__ == '__main__':
+    hall = MovieHall()
+    hall.print_hall()
+    hall.buy_ticket()
+    print(hall.hall_to_string())
